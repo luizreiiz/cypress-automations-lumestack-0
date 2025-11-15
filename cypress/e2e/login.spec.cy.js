104057754773c35da84b7e0a282d957f091beaa3
luizreiiz <luiz.reiiz@gmail.com>
2025-11-15 20:58:01 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,23 +1,32 @@
 describe("Orange HRM Tests", () => {
+  const selectorList = {
+    usernameInput: '[name="username"]',
+    passwordInput: '[name="password"]',
+    loginButton: "button.oxd-button",
+    topBarHeader: ".oxd-text--h6",
+    invalidCredentialsErrorAlert: "div.oxd-alert-content",
+    indexPathname: "/web/index.php/dashboard/index",
+  };
+
   it("Login - Success", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get('[name="username"]').type("Admin");
-    cy.get('[name="password"]').type("admin123");
-    cy.get("button.oxd-button").click();
-    cy.location("pathname").should("eq", "/web/index.php/dashboard/index");
-    cy.get(".oxd-text--h6").contains("Dashboard"); // A way to assert
-    cy.get(".oxd-text--h6").should("have.text", "Dashboard"); // Another way to assert
+    cy.get(selectorList.usernameInput).type("Admin");
+    cy.get(selectorList.passwordInput).type("admin123");
+    cy.get(selectorList.loginButton).click();
+    cy.location("pathname").should("eq", selectorList.indexPathname);
+    cy.get(selectorList.topBarHeader).contains("Dashboard"); // A way to assert
+    cy.get(selectorList.topBarHeader).should("have.text", "Dashboard"); // Another way to assert
   });
   it("Login - Fail", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get('[name="username"]').type("Fail");
-    cy.get('[name="password"]').type("Fail");
-    cy.get("button.oxd-button").click();
-    cy.get("div.oxd-alert-content").should(
+    cy.get(selectorList.usernameInput).type("Fail");
+    cy.get(selectorList.passwordInput).type("Fail");
+    cy.get(selectorList.loginButton).click();
+    cy.get(selectorList.invalidCredentialsErrorAlert).should(
       "have.class",
       "oxd-alert-content--error"
     );
