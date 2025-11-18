5183eb4cc28c0f17ab20115662036435b88e3c5a
luizreiiz <luiz.reiiz@gmail.com>
2025-11-17 22:00:05 -0300

    Refactor user spec to include input fields for user info update

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -9,7 +9,11 @@ describe("Orange HRM Tests", () => {
     // topBarHeader: ".oxd-topbar-header-title",
     dashboardGrid: ".orangehrm-dashboard-grid",
     myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameInput: 'input[name="firstName"]',
+    middleNameInput: 'input[name="middleName"]',
+    lastNameInput: 'input[name="lastName"]',
     invalidCredentialsErrorAlert: "div.oxd-alert-content",
+    genericInput: ".oxd-input--active",
   };
 
   it.only("User Info Update - Success", () => {
@@ -22,6 +26,12 @@ describe("Orange HRM Tests", () => {
     // cy.get(selectorList.topBarHeader).should("have.text", "Dashboard"); // Another unreliable way of using assert
     cy.get(selectorList.dashboardGrid).should("be.visible");
     cy.get(selectorList.myInfoButton).click();
+    cy.get(selectorList.firstNameInput).clear().type("Stanley");
+    cy.get(selectorList.middleNameInput).clear().type("Michael");
+    cy.get(selectorList.lastNameInput).clear().type("Moore");
+    cy.get(selectorList.genericInput).eq(3).clear().type("123456");
+    cy.get(selectorList.genericInput).eq(4).clear().type("654321");
+    cy.get(selectorList.genericInput).eq(5).clear().type("11abc22def333");
   });
   it("Login - Fail", () => {
     cy.visit("/auth/login");
