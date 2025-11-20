738d13144884628379a8e793307017ffe55b47e7
luizreiiz <luiz.reiiz@gmail.com>
2025-11-19 22:13:25 -0300

    Refactor user info update test to use selector variables for dropdown options and radio inputs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -17,6 +17,8 @@ describe("Orange HRM Tests", () => {
     // dateInput: '[placeholder="yyyy-dd-mm"]',
     dateCloseButton: ".--close",
     genericDropdown: ".oxd-select-text--after",
+    dropdowOption: ".oxd-select-option",
+    radioInput: ".oxd-radio-input",
     submitButton: "[type='submit']",
   };
 
@@ -39,16 +41,16 @@ describe("Orange HRM Tests", () => {
     cy.get(selectorList.genericInput).eq(6).clear().type("2030-01-06"); // License Expiry Date
     cy.get(selectorList.dateCloseButton).click(); // Close the date picker
     cy.get(selectorList.genericDropdown).eq(0).click(); // Nationality
-    cy.get(".oxd-select-option").eq(26).click(); // Select "Brazilian"
+    cy.get(selectorList.dropdowOption).eq(26).click(); // Select "Brazilian"
     cy.get(selectorList.genericDropdown).eq(1).click(); // Marital Status
-    cy.get(".oxd-select-option").eq(2).click(); // Select "Married"
+    cy.get(selectorList.dropdowOption).eq(2).click(); // Select "Married"
     cy.get(selectorList.genericInput).eq(8).clear().type("1981-10-08"); // Birthday
-    cy.get(".oxd-radio-input").eq(0).click(); // Select "Female"
+    cy.get(selectorList.radioInput).eq(1).click(); // Select "Female"
     cy.get(selectorList.submitButton).eq(0).click(); // Save Button - Personal Details
     cy.get("body").should("contain.text", "Successfully Updated"); // Success Alert
     cy.get(".oxd-toast-close");
     cy.get(selectorList.genericDropdown).eq(2).click(); // Blood Type
-    cy.get(".oxd-select-option").eq(6).click(); // Select "O-"
+    cy.get(selectorList.dropdowOption).eq(6).click(); // Select "O-"
     cy.get(selectorList.genericInput).eq(9).clear().type("5555"); // Test_Field
     cy.get(selectorList.submitButton).eq(0).click(); // Save Button - Custom Fields
     cy.get("body").should("contain.text", "Successfully Updated"); // Success Alert
