05387bb590d1afe9afd567c7a30be0ed89d074a3
luizreiiz <luiz.reiiz@gmail.com>
2025-11-23 11:59:22 -0300

    Add comments to user info update test for clarity

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -18,16 +18,16 @@ describe("Orange HRM Tests", () => {
         );
         dashboardPage.verifyDashboardPage();
         menuPage.accessMyInfo();
-        myInfoPage.updateEmployeeFullName("Mary", "Jane", "Watson");
-        myInfoPage.updateEmployeeIds("123456", "654321");
-        myInfoPage.updateDriverLicense("111222333", "2030-01-06");
+        myInfoPage.updateEmployeeFullName("Mary", "Jane", "Watson"); // Fill First, Middle and Last Name
+        myInfoPage.updateEmployeeIds("123456", "654321"); // Fill Employee ID and Other ID
+        myInfoPage.updateDriverLicense("111222333", "2030-01-06"); // Fill Driver License Number and License Expiry Date
         myInfoPage.updateNationality(27); // Select "English"
         myInfoPage.updateMaritalStatus(2); // Select "Married"
-        myInfoPage.updateDateOfBirth("1999-03-02");
+        myInfoPage.updateDateOfBirth("1999-03-02"); // Fill Date of Birth
         myInfoPage.updateGender(1); // Select "Female"
-        myInfoPage.clickSubmitButton(0);
+        myInfoPage.clickSubmitButton(0); // Save Personal Details
         myInfoPage.updateBloodType(3, "5555"); // Select "B+" and fill Test_Field
-        myInfoPage.clickSubmitButton(1);
+        myInfoPage.clickSubmitButton(1); // Save Custom Fields
     });
     it("Login - Fail", () => {
         loginPage.accessLoginPage();
